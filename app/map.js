9c9a1440f4095e7a5b80d8322846786568af7893
j.c.sackett <j.c.sackett@gmail.com>
2015-02-07 23:14:33 -0500

    Add basic control input * Get source and destination, plot line.

--- app/map.js
+++ app/map.js
@@ -1,17 +1,59 @@
 var MapView = Backbone.View.extend({
+
+  id: 'page',
+  tagName: 'div',
+  events: {
+    'submit #nav': 'update_args'
+  },
+
   initialize: function(opts) {
     if (opts) {
       this.center = opts.center;
       this.zoom = opts.zoom;
       this.container = opts.container;
     }
-  },   
+  },
+
+  update_args: function(e) {
+    e.preventDefault();
+    if (this.flightPath) {
+      this.flightPath.setMap(null);
+    }
+    var src = e.target[0].value,
+        dest = e.target[1].value;
+
+    src = src.split(',');
+    dest = dest.split(',');
+    src = {
+      lat:  parseFloat(src[0]),
+      lng:  parseFloat(src[1])
+    };
+    dest = {
+      lat:  parseFloat(dest[0]),
+      lng:  parseFloat(dest[1])
+    };
+
+    var flightPathCoords = [
+      new google.maps.LatLng(src.lat, src.lng),
+      new google.maps.LatLng(dest.lat, dest.lng)
+    ];
+    this.flightPath = new google.maps.Polyline({
+      path: flightPathCoords,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+    this.flightPath.setMap(this.g_map);
+    this.g_map.setCenter(src);
+  },
 
   render: function() {
     var mapOptions = {
       center: this.center,
-      zoom: this.zoom 
+      zoom: this.zoom
     };
-    var g_map = new google.maps.Map(this.container, mapOptions);
+    var container = this.$('#map-canvas')[0];
+    this.g_map = new google.maps.Map(container, mapOptions);
   }
 });
