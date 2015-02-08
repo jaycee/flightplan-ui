5557bbca4ab89911821350042d363229a6ce0ad5
j.c.sackett <j.c.sackett@gmail.com>
2015-02-08 18:54:26 -0500

    Break up the route into points based on speed and interval.

--- app/map.js
+++ app/map.js
@@ -20,7 +20,8 @@ var MapView = Backbone.View.extend({
       this.flightPath.setMap(null);
     }
     var src = e.target[0].value,
-        dest = e.target[1].value;
+        dest = e.target[1].value,
+        speed = e.target[2].value;
 
     src = src.split(',');
     dest = dest.split(',');
@@ -37,15 +38,40 @@ var MapView = Backbone.View.extend({
       new google.maps.LatLng(src.lat, src.lng),
       new google.maps.LatLng(dest.lat, dest.lng)
     ];
+    //TODO don't hardcode speed and interval
+    var points = this.get_points(flightPathCoords[0], flightPathCoords[1], 60, 2)    
     this.flightPath = new google.maps.Polyline({
-      path: flightPathCoords,
+      path: points,
       geodesic: true,
       strokeColor: '#FF0000',
       strokeOpacity: 1.0,
       strokeWeight: 2
     });
+    _.each(points, function(point) {
+      debugger;
+      var marker = new google.maps.Marker({
+          position: point,
+      })
+      marker.setMap(this.g_map);
+    });
     this.flightPath.setMap(this.g_map);
     this.g_map.setCenter(src);
+  },
+
+  get_points: function(src, dest, speed, interval) {
+    var distance = google.maps.geometry.spherical.computeDistanceBetween(
+        src, dest, 3959);
+    var interval_speed = speed * interval;
+    var steps = parseInt(distance / interval_speed, 10);
+    var heading = google.maps.geometry.spherical.computeHeading(src, dest);
+    var points = [src];
+    for(var i=0; i<steps; i++) {
+      var point = google.maps.geometry.spherical.computeOffset(
+          src, interval_speed, 3959);
+      points.push(point);
+    }
+    points.push(dest);
+    return points;
   },
 
   render: function() {
